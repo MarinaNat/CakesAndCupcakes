e15486c5bcc7c014af9fd94380a4aaa41114ab3f
Marina Natalina <Kiska21mk@ya.ru>
2023-02-19 19:36:41 +0300

    first-screen

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CakesAndCupcakes
+
+gh-page:  https://marinanat.github.io/CakesAndCupcakes/
+
+Верстка сайта IT компании по макету: https://www.figma.com/file/Zb24i0LQcb0VhqK1jzW3Qx/%D0%9C%D0%B0%D0%BA%D0%B5%D1%82-Landing-Page-%D0%B4%D0%BB%D1%8F-Figma-(%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD)-(Community)-(Copy)-(Copy)?node-id=0%3A328&t=DSWdWDmoPfDKuqJ6-0
---
+++ js/script.js
@@ -0,0 +1,56 @@
+// открытие и закрытие бургер-меню
+const iconMenu = document.querySelector('.first-screen__burger');
+const menuBody = document.querySelector('.first-screen__menu');
+if (iconMenu) {
+	iconMenu.addEventListener('click', function (e) {
+		document.body.classList.toggle('_lock')
+		iconMenu.classList.toggle('_active')
+		menuBody.classList.toggle('_active')
+	})
+}
+
+// скрол при нажатии на меню
+const menuLinks = document.querySelectorAll('.first-screen__link[data-goto]')
+
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener('click', onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('.first-screen__menu').offsetHeight;
+
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock')
+				iconMenu.classList.remove('_active')
+				menuBody.classList.remove('_active')
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			})
+			e.preventDefault()
+		}
+	}
+}
+
+
+// Применение класса "ibg"
+function ibg() {
+
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+
+ibg();
+
+
+
